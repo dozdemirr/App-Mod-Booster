97994eb7920b2c825ff425477db75a042132e6ee
Deniz Ozdemir <88308709+dozdemirr@users.noreply.github.com>
2026-04-14 22:24:55 +0100

    Delete app/ExpenseManagement directory

--- app/ExpenseManagement/Models/ExpenseSummary.cs
+++
@@ -1,8 +0,0 @@
-namespace ExpenseManagement.Models;
-
-public class ExpenseSummary
-{
-    public string StatusName { get; set; } = string.Empty;
-    public int Count { get; set; }
-    public decimal TotalAmountGBP { get; set; }
-}
